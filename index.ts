b647faa862177fcf7fea0b0261900b211f07e67e
frankdulko <79063207+frankdulko@users.noreply.github.com>
2025-08-13 12:41:08 -0400

    load a list of movies in db

--- index.ts
+++ index.ts
@@ -1,1 +1,35 @@
-console.log("Hello, World!");
+const data = require("./movies.json"); // Path relative to this file
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+
+    const db = client.db("moviemoji");
+    const collection = db.collection("movies");
+
+    // 3. Insert each object as a document
+    for (const doc of data) {
+      await collection.insertOne(doc);
+      console.log(`Inserted: ${doc.title}`);
+    }
+    console.log("All documents inserted!");
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
